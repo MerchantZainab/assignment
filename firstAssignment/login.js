8a06890c8e9aaae884c39b3112f232b5cb18eab2
MerchantZainab <zainab.armerchant@gmail.com>
2019-05-12 00:26:27 +0500

    adding new files

---
+++ firstAssignment/login.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var fs = require("fs");
+
+
+var app = express();
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+// app.get('/',function(req,res){
+//   res.sendFile('index.html',{root:path.join(__dirname,'/public')});
+// });
+
+app.use(express.static("./public"));
+
+app.post('/',function(req,res){
+// res.end(JSON.stringify(req.body));
+if(req.body.username=='z' && req.body.password=='z'){
+console.log('login'+__filename);
+
+res.sendFile('todo.html',{root:path.join(__dirname,'./public/innerpages')});
+}
+else{
+  console.log('fail');
+  res.end(`fail`);
+
+}
+});
+
+app.listen(3000,function()
+{
+  console.log('Listening to port 3000');
+});
